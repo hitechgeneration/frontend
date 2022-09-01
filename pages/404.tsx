2ce8a374a32157ccc866c0070e539595800de45b
rei <reiballa@gmail.com>
2022-09-01 12:24:22 +0200

    dumb error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function NotFound() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div>Ke humb pak o shoku</div>
